bb90020e775166b24105f75f0e330fa14f7609f7
mustafaAtefmustafa <mustafaatef761@gmail.com>
2021-12-08 06:44:34 +0200

    black and white pawn moves

--- chess.py
+++ chess.py
@@ -18,6 +18,9 @@ class GameState():
             ["white_rook", "white_knight", "white_bishop", "white_queen",
                 "white_King", "white_bishop", "white_knight", "white_rook"]
         ]
+        # A dictionary to map the getting moves functions to their right piece.
+        self.get_moves_functions = {'p': self.get_pawn_moves, 'r': self.get_rook_moves, 'k': self.get_knight_moves, 
+                                    'b': self.get_bishop_moves, 'q': self.get_queen_moves, 'K': self.get_king_moves}
         self.white_to_move = True
         self.move_log = []
 
@@ -50,27 +53,40 @@ class GameState():
         for row in range(len(self.board)):
             for col in range(len(self.board[row])):
                 turn = self.board[row][col][0] # Getting the color of the piece ('b' for black or 'w' for white or '-' for empty)
-                if (turn == 'w' and self.white_to_move) and (turn == 'b' and not self.white_to_move):
+                if (turn == 'w' and self.white_to_move) or (turn == 'b' and not self.white_to_move):
                     piece = self.board[row][col][6] # getting the piece as ('r' for rook...etc.) notice 'K' for king and 'k' for knight.
-                    # checking which piece we have then call the function that generates its possible moves.
-                    if piece == 'p':
-                        self.get_pawn_moves(row, col, moves)
-                    elif piece == 'r':
-                        self.get_rook_moves(row, col, moves)
-                    elif piece == 'k':
-                        self.get_knight_moves(row, col, moves)
-                    elif piece == 'b':
-                        self.get_bishop_moves(row, col, moves)
-                    elif piece == 'q':
-                        self.get_queen_moves(row, col, moves)
-                    elif piece == 'K':
-                        self.get_king_moves(row, col, moves)
+                    # Calling the function that generates the piece's possible moves.
+                    self.get_moves_functions[piece](row, col, moves)
         return moves
 
     # Get all pawn possible moves for the pawn in location row, col and add them to list.
     def get_pawn_moves(self, row, col, moves):
-        pass
-
+        # White pawns moves.
+        if self.white_to_move: # Checking if it's white's turn to move.
+            if self.board[row - 1][col] == '--':  # 1 square pawn advance.
+                moves.append(Move((row, col), (row - 1, col), self.board))
+                if row == 6 and self.board[row - 2][col] == '--':  # 2 square pawn advance.
+                    moves.append(Move((row, col), (row - 2, col), self.board))
+            # White pawns captures. "Pawns can only capture diagonally one square to the right or the left."
+            if col - 1 >= 0: # Making sure that we don't have any negative numbers.
+                if self.board[row - 1][col - 1][0] == 'b':  # Capturing a black pawn to the left. 
+                    moves.append(Move((row, col), (row - 1, col - 1), self.board))
+            if col + 1 <= 7: # Making sure we don't cross the borders of the board
+                if self.board[row - 1][col + 1][0] == 'b':  # Capturing a black pawn to the right
+                    moves.append(Move((row, col), (row - 1, col + 1), self.board))
+        # Black pawns moves.
+        else:
+            if self.board[row + 1][col] == '--':
+                moves.append(Move((row, col), (row + 1, col), self.board))
+                if row == 1 and self.board[row + 2][col] == '--':
+                    moves.append(Move((row, col), (row + 2, col), self.board))
+            # Black pawns captures.
+            if col - 1 >= 0: # Making sure that we don't have any negative numbers.
+                if self.board[row + 1][col - 1][0] == 'w':  # Capturing a white pawn to the left. 
+                    moves.append(Move((row, col), (row + 1, col - 1), self.board))
+            if col + 1 <= 7: # Making sure we don't cross the borders of the board
+                if self.board[row + 1][col + 1][0] == 'w':  # Capturing a white pawn to the right
+                    moves.append(Move((row, col), (row + 1, col + 1), self.board))
 
     # Get all rook possible moves for the rook in location row, col and add them to list.
     def get_rook_moves(self, row, col, moves):
